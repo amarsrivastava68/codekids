838ed3e5446fb374238ab3a9bd66d29dce493651
amarsrivastava68 <amarsrivastava68@gmail.com>
2023-06-29 15:33:16 +0530

    final commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>CodeKids</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/amar__srivastava__" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/amarsrivastava68" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
